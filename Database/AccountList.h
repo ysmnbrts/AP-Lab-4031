926feef1f8eee1e2be29272da5b6e2b591ad5b1f
ysmnbrts <154685421+ysmnbrts@users.noreply.github.com>
2024-10-27 23:54:02 +0330

    new sort functions for account list

--- Database/AccountList.h
+++ Database/AccountList.h
@@ -24,6 +24,36 @@ public:
 	
 	void swap(Account& acc1, Account& acc2);
 	void print();
+	Account *merge(Account *first, Account *second) 
+	{
+	
+	  if (first == nullptr) return second;
+	if (second == nullptr) return first;
+	{
+	    first->next = merge(first->next, second);
+	    return first;
+	}
+	else {
+	    second->next = merge(first, second->next);
+	    return second;
+	}
+	}
+	
+	Account *MergeSort(Account *head) 
+	{
+	if (head == nullptr || head->next == nullptr)
+	    return head;
+	
+	// Split the list into two halves
+	Node *second = split(head);
+	
+	// Recursively sort each half
+	head = MergeSort(head);
+	second = MergeSort(second);
+	
+	// Merge the two sorted halves
+	return merge(head, second);
+	}
 private:
 	Account* head;
 	int size;
